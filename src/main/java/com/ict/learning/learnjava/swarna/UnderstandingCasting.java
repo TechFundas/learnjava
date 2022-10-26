7a8415cd01b553947205459f22e4ff08202e4219
spenumallu <swarna.penumallu@gmail.com>
2022-10-26 12:14:54 -0400

    Create UnderstandingCasting.java

---
+++ src/main/java/com/ict/learning/learnjava/swarna/UnderstandingCasting.java
@@ -0,0 +1,20 @@
+package com.ict.learning.learnjava.swarna;
+
+public class UnderstandingCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String numValue = "70";
+		int numValueInt = Integer.parseInt(numValue);
+		System.out.println(numValue);
+		
+		int  sumx = 38;
+		String fvalueString = "38";
+		System.out.println(fvalueString);
+		
+		
+		
+         
+	}
+
+}
