dc1271b19c4fee1453c7818d365b9f99af5e389e
srivanikl <116591562+srivanikl@users.noreply.github.com>
2022-10-26 16:33:47 -0700

    encapsulation casting assignment

---
+++ src/main/java/com/ict/learning/learnjava/srivani/RestaurentApp.java
@@ -0,0 +1,40 @@
+package com.ict.learning.learnjava.srivani;
+
+public class RestaurentApp {
+
+	private String name;
+	private int phonenum;
+	private String Address;
+	private String food;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPhonenum() {
+		return phonenum;
+	}
+	public void setPhonenum(int phonenum) {
+		this.phonenum = phonenum;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	public String getFood() {
+		return food;
+	}
+	public void setFood(String food) {
+		this.food = food;
+	}
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/main/java/com/ict/learning/learnjava/srivani/UnderstandingEncapsulation.java
@@ -0,0 +1,28 @@
+package com.ict.learning.learnjava.srivani;
+
+public class UnderstandingEncapsulation {
+	private int age;
+	private String gender;
+	
+
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+	public String getGender() {
+		return gender;
+	}
+
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+
+}
