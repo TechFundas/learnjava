75414b5bead68819a30a7d50f96fadcf24aee6d7
ojdipti <116591597+chaitanyaoj@users.noreply.github.com>
2022-10-26 21:41:05 -0400

    ResAppChanges
    Restaurant App Change

---
+++ src/main/java/com/ict/learning/learnjava/chaitanya/RestaurantApp.java
@@ -0,0 +1,42 @@
+package com.ict.learning.learnjava.chaitanya;
+
+public class RestaurantApp {
+	
+	private String name;
+	private String address;
+	private int phonenumber;
+	private String food;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address; 
+	}
+	
+	public int getphonenumber() {
+		return phonenumber;
+	}
+	
+	public void setphonenumber(int phonenumber) {
+		this.phonenumber = phonenumber; 
+	}
+	
+	public String getfood() {
+		return food;
+	}
+	
+	public void setfood(String food) {
+		this.food = food; 
+	}
+
+}
